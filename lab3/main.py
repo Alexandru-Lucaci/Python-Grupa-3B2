ea6d534a8818b52330ace90c24b3a7ac82648746
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-10-22 18:07:23 +0300

    working ex 5

--- lab3/main.py
+++ lab3/main.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 
 
 def ex1( a,b):
@@ -19,19 +20,24 @@ def ex2(stringwithProp):
                 letterDictionary[letter] = 1
 
     print(letterDictionary)
-
+# if isinstance(element, Iterable) and type(element) != str:
 def recursivityEx3(item):
-    final=[]
-    if type(item) is dict:
-        for key in item:
-            final+=recursivityEx3(item[key])
+    if isinstance(item, Iterable) and type(item) != str:
+        final=[]
+        if type(item) is dict:
+            for key in item:
+                final+=recursivityEx3(item[key])
+        else:
+            for i in item:
+                final+= recursivityEx3(i)
+        return final
     else:
-        for i in item:
-            final+= recursivityEx3(i)
-    return final
-
+        return [item]
 def ex3(dictionar1, dictionar2):
-    return list(set(recursivityEx3(dictionar1)) ^ set(recursivityEx3(dictionar2)))
+
+    if (list(set(recursivityEx3(dictionar1)) ^ set(recursivityEx3(dictionar2)))) == []:
+        return True
+    return False
 
 
 def ex4(tag, content, **parameters):
@@ -41,8 +47,38 @@ def ex4(tag, content, **parameters):
     tagCreation += ">" + content + "</" + tag + ">"
     return tagCreation
 
-def ex5():
-    print("ex")
+def validate_dict(tupleSet, dictionary):
+    keys = [item[0] for item in tupleSet if item[0].startswith("key")]
+    print(keys)
+    for key in dictionary.items():
+        if key[0] not in keys:
+            return False
+    for key in dictionary.items():
+        rule = [x for x in tupleSet if x[0] == key[0]]
+        print(rule)
+
+        if rule[0][1] != '':
+            if not key[1].startswith((rule[0][1])):
+                return False
+        if rule[0][3] != '':
+            if not key[1].endswith((rule[0][3])):
+                return False
+        if rule[0][2] != '':
+            if not rule[0][2] in key[1]:
+                return False
+            if key[1].startswith(rule[0][2]):
+                return False
+            if key[1].endswith(rule[0][2]):
+                return False
+
+
+
+
+    return True
+
+print(validate_dict([("key1", "","inside",""), ("key2", "start", "middle", "winter")], {"key1": "come inside, it's too cold out", "key2": "start this middle is not valid winter"}))
+def ex5(tupleSet, dictionary):
+    validate_dict(tupleSet, dictionary)
 #     idk yet
 def ex6(listA):
     setA = set(listA)
@@ -120,7 +156,7 @@ if __name__ == '__main__':
         elif exercitiu == 3:
             dict1={1:2,3:4,5:6}
             dict2={1:2,3:4,5:6}
-            ex3(dict1,dict2)
+            print(ex3(dict1,dict2))
         elif exercitiu == 4:
             print(ex4("a", "saluuut", href="http://python.org", _class="my-link", id="someid"))
         elif exercitiu == 5:
