254e34e6a0362cd7e413e3216058364fc734064e
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-11-07 11:10:39 +0200

    update lab5

--- lab5/main.py
+++ lab5/main.py
@@ -1,4 +1,4 @@
-import utils
+# import utils
 
 sumOfNums = lambda *y, **x: sum(x.values())
 
@@ -11,23 +11,27 @@ def ex2(*y, **x):
     print(sumOfNums(*y, **x))
     print(functieAdunare(*y, **x))
 
+
 def ex3(_list):
-#     return a list with all the chars in the string
-    finalList =[]
-    vowels = ['a','e','i','o','u']
-    for i in range (0,(len(_list))):
+    #     return a list with all the chars in the string
+    finalList = []
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    for i in range(0, (len(_list))):
         if _list[i] in vowels:
             finalList.append(_list[i])
     return finalList
 
+
 def ex3Filter(string):
-    vocale = ['a','e','i','o','u']
+    vocale = ['a', 'e', 'i', 'o', 'u']
     return list(filter(lambda element: element in vocale, string))
 
-ex3ListLambda = lambda string: [element for element in string if element in ['a','e','i','o','u']]
+
+ex3ListLambda = lambda string: [element for element in string if element in ['a', 'e', 'i', 'o', 'u']]
+
 if __name__ == '__main__':
     inputul = int(input(" element : "))
-    while (inputul != 0):
+    while inputul != 0:
         if inputul == 2:
             ex2(a=1, b=2, c=3, d=4, e=5)
         elif inputul == 3:
@@ -36,5 +40,3 @@ if __name__ == '__main__':
             print(ex3ListLambda("salut ce faci"))
         inputul = int(input(" element : "))
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
