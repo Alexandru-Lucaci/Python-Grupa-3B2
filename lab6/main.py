4fcb641948f23df30bad1ebb1e1b4c0e2d01c373
Alexandru Lucaci B2 <lucacialexandru18@gmail.com>
2022-11-24 16:44:24 +0200

    Update main.py

--- lab6/main.py
+++ lab6/main.py
@@ -12,6 +12,9 @@ def ex1(string_words):
 
     return list_words
 
+def ex1RE(string_words):
+    import re
+    return re.findall(r'\w+', string_words)
 def ex2(regex_string,text_string,x):
     # and returns those long-length x substrings that match the regular expression
     import re
@@ -21,16 +24,13 @@ def ex2(regex_string,text_string,x):
             list_words.append(match.group())
     return list_words
 
-print(ex2(r'\d+', 'abc123def456ghi789', 3))
-
-# Write a function that receives as a parameter a string of text characters and a list of regular expressions and returns a list of strings that match on at least one regular expression given as a parameter.
 def ex3(string_words, list_regex):
     import re
     list_words = []
     for regex in list_regex:
         list_words.extend(re.findall(regex, string_words))
     return list_words
-# Write a function that receives as a parameter the path to an xml document and an attrs dictionary and returns those elements that have as attributes all the keys in the dictionary and values ​​the corresponding values. For example, if attrs={"class": "url", "name": "url-form", "data-id": "item"} the items selected will be those tags whose attributes are class="url" si name="url-form" si data-id="item".
+
 def ex4(path_xml, attrs):
     import sys
     import os
@@ -77,9 +77,6 @@ def ex5(path_xml, attrs):
         if len(lista)>0:
             retrn_list.extend(lista)
     return retrn_list
-exx= ex5("D:\\git\\GitHub\PythonUdemy\\text.html", {'class': 'ccfdf', 'name': 'url-form', 'data-id': 'item'})
-for i in exx:
-    print(i)
 
 
 def ex6(text_param):
@@ -96,7 +93,7 @@ def ex6(text_param):
             word = word[:-1]
         # verific daca am cumva
         if re.match(r'[aeiouAEIOU]+\w*[aeiouAEIOU]$', word):
-            new_word ='';
+            new_word =''
             for index,i in enumerate(word):
                 if (index+1)%2==1:
                     new_word+='*'
@@ -107,10 +104,10 @@ def ex6(text_param):
         list_words+=' '
     return list_words
 
-print(ex4("D:\\git\\GitHub\PythonUdemy\\text.html", {"class": "ccfdf"}))
+#
 text = "Lorem Ipsum is simply dummy ana text are mere of the printing and andreea analiza apa, typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
-print(ex6(text))
+# print(ex6(text))
 
 def ex7(string_text):
     import re
@@ -119,7 +116,7 @@ def ex7(string_text):
     else:
         if len(string_text) == 13:
             # print('here')
-            if re.match(r'[12]|[56]\d{12}',string_text):
+            if re.match(r'[12]|[56]|[34]|[78]\d{12}',string_text):
                 # print('match')
                 if int(string_text[3:5])>12:
                     raise Exception('Luna invalida')
@@ -128,7 +125,7 @@ def ex7(string_text):
                 return True
             else:
                 raise Exception('Cod invalid')
-        raise Exception('Numai mai mic decat 13 cifre')
+        raise Exception('numar mai mic decat 13 cifre')
 
 def ex8(directory, regular_expressions):
     import os
@@ -144,23 +141,21 @@ def ex8(directory, regular_expressions):
             boolean_and_text = False
             for i in regular_expressions:
                 if re.match(i, file) :
-                    print('matched 0')
+                    # print('matched 0')
                     # print(file)
                     f = open(os.path.join(directory,file), "r")
                     text = f.read()
-                    print(text)
+                    # print(text)
                     f.close()
-                    print(i)
+                    # print(i)
                     if re.match(i, text):
-                        print ('matched')
+                        # print ('matched')
                         boolean_and_text = True
                         boolean_just_the_name = True
                         # print('>>'+file)
                     else:
                         boolean_just_the_name = True
-
-                        # print(file)
-            if boolean_and_text:
+            if boolean_and_text == True:
                 print('>>'+file)
             else:
                 if boolean_just_the_name:
@@ -169,14 +164,51 @@ def ex8(directory, regular_expressions):
         # else:
             # ex8(os.path.join(directory,file),regular_expressions)
             # raise Exception(f'{file} Not a file')
-ex8('D:\\javascript\\tutorial', [r'.*\.txt$', r'.*\.js$', r'.*\.html$'])
+# ex8('D:\\javascript\\tutorial', [r'.*\.txt$', r'.*\.js$', r'.*\.html$'])
 if __name__ == '__main__':
-    print(ex1('Ana are 12 3     s      mere'))
+
     try:
-        print(len('6221131234456'))
-        if ex7('1311223311219'):
-            print('CNP valid')
-        else:
-            print('CNP invalid')
+        if __name__ == '__main__':
+            exercitiu = 0
+            while exercitiu != -99:
+                exercitiu = int(input("Alege un exercitiu"))
+                if exercitiu == 1:
+                    print(ex1('Ana are 12 3     s      mere'))
+                    print(ex1('Ana are 12 3     s      mere'))
+                elif exercitiu == 2:
+                    print(ex2(r'\d+', 'abc123def456ghi789', 3))
+                elif exercitiu == 3:
+                    print(ex3('ana are mere si pere si nu are banane', ['[a-z]+', '[0-9]+']))
+                elif exercitiu == 4:
+                    print(ex4("D:\\git\\GitHub\PythonUdemy\\text.html", {"class": "ccfdf"}))
+                elif exercitiu == 5:
+                    exx = ex5("D:\\git\\GitHub\PythonUdemy\\text.html",
+                              {'class': 'ccfdf', 'name': 'url-form', 'data-id': 'item'})
+                    for i in exx:
+                        print(i)
+
+                elif exercitiu == 6:
+                    print(ex6(text))
+                elif exercitiu == 7:
+                    if ex7('1311223311219'):
+                        print('CNP valid')
+                    else:
+                        print('CNP invalid')
+                elif exercitiu == 8:
+                    ex8('D:\\javascript\\tutorial', [r'.*\.txt$', r'.*\.js$', r'.*\.html$'])
+
+                else:
+                    break
+
     except Exception as e:
         print(e)
+
+    # print(ex1('Ana are 12 3     s      mere'))
+    # try:
+    #     # print(len('6221131234456'))
+    #     if ex7('1311223311219'):
+    #         print('CNP valid')
+    #     else:
+    #         print('CNP invalid')
+    # except Exception as e:
+    #     print(e)
